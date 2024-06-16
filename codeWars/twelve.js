4b79614fb5bfb70220680df8ecf573d10c5e15fd
Refagi <renogivaro@gmail.com>
2024-06-16 17:03:50 +0700

    twelve

---
+++ codeWars/twelve.js
@@ -0,0 +1,27 @@
+
+function hammingWeight(x) {
+  let i = 0
+  while (x > 0) {
+    i += x & 1
+    x >>= 1
+  }
+  return i
+}
+console.log(hammingWeight(2))
+
+
+function pillars(numPill, dist, width) {
+  let result = ''
+  if (numPill > 1){
+      let result1 = (numPill - 1) * (dist * 100)
+      let result2 = (numPill - 2) * width
+      result += result1 + result2
+  } else {
+      result += 0
+  }
+  return Number(result)
+  // your code here
+}
+console.log(pillars(1, 10, 10))//0
+console.log(pillars(2, 20, 25))//2000
+console.log(pillars(11, 15, 30))//15270
