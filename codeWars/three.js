2d960b6f9f7036ba6696c021679e2d1f34f835b7
Refagi <renogivaro@gmail.com>
2024-06-16 16:54:47 +0700

    three

---
+++ codeWars/three.js
@@ -0,0 +1,21 @@
+
+ // Write a function "greet" that returns "hello world
+ function greet(){
+  return 'hello world!'
+}
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+function booleanToString(b){
+  if (b === true){
+   return 'true'
+ } else {
+   return 'false'
+ }
+//your code here
+}
+console.log(booleanToString(true))
+console.log(booleanToString(false))
