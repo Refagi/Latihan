f8ca39de3d038659f094ecc17db708190a468d08
Refagi <renogivaro@gmail.com>
2024-09-11 16:54:16 +0700

    add soal

--- leetCode/one.js
+++ leetCode/one.js
@@ -1,35 +1,44 @@
-function premierLeagueStandings(teamStandings) {
-  let arr = []
-  let arr2 = []
-  for (const key in teamStandings){
-      arr.push(teamStandings[key])
+/**
+ * 
+ * Given a string s, find the length of the longest substring without repeating characters.
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+
+
+function lengthOfLongestSubstring (s) {
+  let count = [];
+  let left = 0;
+  let result = 0;
+  for (let right = 0; right < s.length; right++) {
+    if(count[s[right]] !== undefined && count[s[right]] >= left){
+      left = count[s[right]] + 1;
+    }
+    // console.log('left: ', left)
+    count[s[right]] = right;
+    // console.log('right: ', right)
+
+    result = Math.max(result, (right - left + 1));
+    // console.log('result: ', result, '\n')
   }
-  let first = arr[0]
-  for (let j = 0; j < arr.length; j++){
-      if (arr[j] === first){
-          continue
-      }
-   arr2.push(arr[j])
-  }
-  const obj = {1: first}
-  for (let i = 0; i < arr2.length; i++){
-      for (let l = 0; l < arr2.length - 1; l++){
-          if (arr2[l] > arr2[l + 1]){
-              let result = arr2[l]
-              arr2[l] = arr2[l + 1]
-              arr2[l + 1] = result
-          }
-      }
-      
-  }
-  for (let k = 0; k < arr2.length; k++){
-      obj[k + 2] = arr2[k]
-  }
-  return obj
-}
-console.log(premierLeagueStandings({1:'Arsenal'}))
-//{1:'Arsenal'}
-console.log(premierLeagueStandings({2:'Arsenal', 3:'Accrington Stanley', 1:'Leeds United'})) 
-// {1:'Leeds United', 2:'Accrington Stanley', 3:'Arsenal'} 
-console.log(premierLeagueStandings({1:'Leeds United', 2:'Liverpool', 3:'Manchester City', 4:'Coventry', 5:'Arsenal'})) 
-// {1:'Leeds United', 2:'Arsenal', 3:'Coventry', 4:'Liverpool', 5:'Manchester City'} 
+  return result;
+};
+
+
+console.log(lengthOfLongestSubstring("abcabcbb")); //3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); //3
